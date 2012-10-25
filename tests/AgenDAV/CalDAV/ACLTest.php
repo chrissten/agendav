ee878b8e5878bbdd041eda346534d48a93141ed5
Jorge López Pérez <jorge@adobo.org>
2012-10-25 18:53:52 +0200

    Add an ACE generation test

--- tests/AgenDAV/CalDAV/ACLTest.php
+++ tests/AgenDAV/CalDAV/ACLTest.php
@@ -35,4 +35,63 @@ class ACLTest extends \PHPUnit_Framework_TestCase
         $acl->setOptions(array());
     }
 
+    public function testACE1()
+    {
+        // Values: array(occurrences, children)
+        $expected = array(
+            '/ace' => array(1, 2),
+            '/ace/principal' => array(1, 1),
+            '/ace/principal/property' => array(1, 1),
+            '/ace/principal/property/owner' => array(1, 0),
+            '/ace/grant' => array(1, 2),
+            '/ace/grant/privilege' => array(2, null),
+            '/ace/grant/privilege[1]' => array(1, 1),
+            '/ace/grant/privilege[2]' => array(1, 1),
+            '/ace/grant/privilege[1]/read' => array(1, 0),
+            '/ace/grant/privilege[2]/write' => array(1, 0),
+        );
+        $acl = new ACL($this->options1);
+        $d = new \DOMDocument();
+
+        $ace1 = $acl->generateACE($d, 'owner');
+        $this->assertInstanceOf('\DOMElement', $ace1);
+        $d->appendChild($ace1);
+        $xml = $d->saveXML();
+
+        $this->checkXML($expected, $xml);
+    }
+
+    /**
+     * Checks an XML against provided expected array
+     *
+     * @param mixed $expected
+     * @param mixed $xml_text
+     * @access private
+     * @return void
+     */
+    private function checkXML($expected, $xml_text)
+    {
+        $parsed_xml = simplexml_load_string($xml_text);
+
+        foreach ($expected as $path => $values) {
+            list($occurrences, $children) = $values;
+            $xpath = $parsed_xml->xpath($path);
+            $found = count($xpath);
+            $this->assertEquals(
+                $occurrences,
+                $found,
+                'Expected ' . $occurrences . ' occurrences for ' . $path . ', found ' . $found
+            );
+
+            if ($occurrences == 1) {
+                $found_children = $xpath[0]->count();
+                $this->assertEquals(
+                    $children,
+                    $found_children,
+                    'Expected ' . $children . ' children for ' . $path . ', found ' . $found_children
+                );
+            }
+        }
+    }
+
 }
